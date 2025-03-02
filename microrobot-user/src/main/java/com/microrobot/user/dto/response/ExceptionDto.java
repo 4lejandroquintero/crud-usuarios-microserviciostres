b716b6f9cd23bc63ae17a5183baac40105307537
Alejandro Quintero <alejoq96@gmail.com>
2025-03-02 00:30:39 -0500

    feature(msvc-user): Complemento para el manejo de mensajes de éxito y error en el microservicio de user

---
+++ microrobot-user/src/main/java/com/microrobot/user/dto/response/ExceptionDto.java
@@ -0,0 +1,23 @@
+package com.microrobot.user.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ExceptionDto {
+
+    private String message;
+    private List<String> errors;
+
+    public ExceptionDto(String message) {
+        this.message = message;
+    }
+
+    public ExceptionDto(String message, List<String> errors) {
+        this.message = message;
+        this.errors = errors;
+    }
+}
---
+++ microrobot-user/src/main/java/com/microrobot/user/exception/ControllerExceptionHandler.java
@@ -0,0 +1,92 @@
+package com.microrobot.user.exception;
+
+import com.microrobot.user.dto.response.ExceptionDto;
+import com.microrobot.user.exception.entities.EntityNotFoundException;
+import com.microrobot.user.exception.entities.OperationNotAllowedException;
+import com.microrobot.user.exception.entities.UserNotFoundException;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.ValidationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ExceptionDto> handleUnknownException(Exception e) {
+        LOGGER.error("Internal error", e);
+        ExceptionDto exceptionDto = new ExceptionDto(
+                "internal_error",
+                Collections.singletonList(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exceptionDto);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<String> errors = e.getBindingResult().getAllErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        ExceptionDto exceptionDto = new ExceptionDto("Validation errors have occurred", errors);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionDto);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ExceptionDto> handleConstraintViolationException(ConstraintViolationException e) {
+        List<String> errors = e.getConstraintViolations().stream()
+                .map(violation -> violation.getMessage())
+                .collect(Collectors.toList());
+
+        ExceptionDto exceptionDto = new ExceptionDto("Validation errors have occurred", errors);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionDto);
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<ExceptionDto> validationException(ValidationException e) {
+        ExceptionDto exceptionDto = new ExceptionDto(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionDto);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ExceptionDto> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        ExceptionDto exceptionDto = new ExceptionDto("Malformed JSON request");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionDto);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ExceptionDto> handleEntityNotFoundException(EntityNotFoundException e) {
+        ExceptionDto exceptionDto = new ExceptionDto(e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionDto);
+    }
+
+    @ExceptionHandler(OperationNotAllowedException.class)
+    public ResponseEntity<ExceptionDto> handleOperationNotAllowedException(OperationNotAllowedException e) {
+        ExceptionDto exceptionDto = new ExceptionDto(e.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(exceptionDto);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ocurrió un error inesperado: " + e.getMessage());
+    }
+
+}
